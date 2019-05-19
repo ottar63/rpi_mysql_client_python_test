586e04184fdf0d9fcec8b4c2517878c69717f199
Ottar Søfteland <soeftela@online.no>
2019-05-19 20:01:34 +0100

    added  routine to show env and request

--- test_con.py
+++ test_con.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_table import Table,Col
 import pymysql
 import socket
+import os
+import sys
 
 app=Flask(__name__)
 class VisitTable(Table):
@@ -15,12 +17,12 @@ class db_test:
         self.cur=self.db.cursor()
         self.hostname=socket.gethostname()
         self.ip=socket.gethostbyname(socket.getfqdn())
-        sql="INSERT INTO visits (host,ip_address) VALUES ('{}','{}');".format(self.hostname,self.ip)
+        sql="INSERT INTO visits (host,ip_address,date_time) VALUES ('{}','{}',NOW());".format(self.hostname,self.ip)
         #print(sql)
         self.cur.execute(sql)
         self.db.commit()
     def list_visits(self):
-        sql="SELECT id , host ,ip_address  FROM visits ORDER BY id DESC"
+        sql="SELECT id , host ,ip_address, date_time  FROM visits ORDER BY id DESC"
         print(sql)
         self.cur.execute(sql)
         self.visits=[]
@@ -34,6 +36,18 @@ def hello():
     visits=mytest.list_visits()
     visit_table=VisitTable(visits)    
     return render_template('visits.html', visits=visits)
+@app.route('/env')
+def env():
+    env=os.environ
+    print("env:{}".format(env))
+    sys.stdout.flush()
+    return render_template('env.html',env=env)
+@app.route('/req')
+def req():
+    req=request.environ
+    print("req:{}".format(req))
+    sys.stdout.flush()
+    return render_template('req.html',req=req)
 
 
 if __name__ == "__main__":
